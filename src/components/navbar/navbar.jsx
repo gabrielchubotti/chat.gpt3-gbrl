32710b33cfd2b265366c0b2113d96471d904fb06
Gabriel Chu Bottiglieri <gabrielchubotti@gmail.com>
2023-01-12 21:48:29 -0300

    completed navbar

---
+++ src/components/navbar/navbar.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { RIMenu3Line, RiCloseLin} from 'react-icons/ri';
+import './navbar.css';
+import logo from '../../assets/logo.svg'
+
+// BLOCK ELEMENT MODIFIER BEM - NAMING CONVENTION
+
+const Navbar = () => {
+  return (
+    <div className='gpt3__navbar'>
+      <div className='gpt3__navbar-links'>
+        <div className='gpt3__navbar-links_logo'>
+          <img src = {logo} alt = 'logo' />
+        </div>
+        <div className='gpt3__navbar-links_container'>
+          <p><a href = '#home'>Home</a></p>
+          <p><a href = '#wgpt3'>What is GPT3?</a></p>
+          <p><a href = '#possibility'>OpenAI</a></p>
+          <p><a href = '#features'>Case Studies</a></p>
+          <p><a href = '#blog'>Library</a></p>
+        </div>
+      </div>
+      <div className='gpt3__navbar-sign'>
+        <p>Sign in</p>
+        <button type='button'>Sign up</button>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
