f0a134a6daab2abdc4832f1470c753c067adbc69
Gabriel Chu Bottiglieri <gabrielchubotti@gmail.com>
2023-01-12 22:37:55 -0300

    starting header implementation

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -4,7 +4,16 @@ import './header.css';
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className='gpt3__header section__padding' id='home'>
+      <div className='gpt3__header-content'>
+        <h1 className='gradient__text'> Let's Build Something Amazing with GPT-3 OpenAI</h1>
+        <div className='gpt3__header-content__input'>
+          <input type='email' placeholder='Your Email Address'></input>
+          <button type='button'>Get Started</button>
+        </div>
+      </div>
+
+    </div>
   )
 }
 
