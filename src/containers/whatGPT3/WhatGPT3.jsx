6b8ca52343e7edc8cd3b3cdf3e5e1bbd641bdff0
Gabriel Chu Bottiglieri <gabrielchubotti@gmail.com>
2023-01-13 23:08:30 -0300

    started structuring whatgpt3 section

--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -1,10 +1,26 @@
 import React from 'react';
+import {Feature} from '../../components/feature/Feature'
 import './whatGPT3.css';
 
 
 const WhatGPT3 = () => {
   return (
-    <div>WhatGPT3</div>
+    <div className='gpt3__whatgpt3 section__margin' id='wgpt3'>
+      <div className='gpt3__whatgpt3-feature'>
+        <Feature />
+      </div>
+      <div className='gpt3__whatgpt3-heading'>
+        <h1 className='gradient-text'>
+          The possibilities are beyond imagination
+        </h1>
+        <p>Explore the Library</p>
+      </div>
+      <div className='gpt3__whatgpt3-container'>
+        <Feature />
+        <Feature />
+        <Feature />
+      </div>
+    </div>
   )
 }
 
