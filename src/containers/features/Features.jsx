1f23f59ebdee381357b6bacd2fdd7b0660ef597f
Gabriel Chu Bottiglieri <gabrielchubotti@gmail.com>
2023-01-15 21:24:02 -0300

    adding changrs to third section of the main page

--- src/containers/features/Features.jsx
+++ src/containers/features/Features.jsx
@@ -1,9 +1,36 @@
 import React from 'react';
 import './features.css';
 
+const featuresData = [
+{
+  title: "dsfsdfsd",
+  text: "fasdfsadfasdfasdfasdfasdfasdasd"
+},
+{
+  title: "dsfsdfsd",
+  text: "fasdfsadfasdfasdfasdfasdfasdasd"
+},
+{
+  title: "dsfsdfsd",
+  text: "fasdfsadfasdfasdfasdfasdfasdasd"
+},
+{
+  title: "dsfsdfsd",
+  text: "fasdfsadfasdfasdfasdfasdfasdasd"
+},
+]
+
 const Features = () => {
   return (
-    <div>Features</div>
+    <div className='gpt3__features section__padding' id='features'>
+      <div className='gpt3__features-heading'>
+        <h1 className='gradient__text'>The future is now, and you just need to realize it. Step into the future today and make it happen. </h1>
+      <p>Request early access to get started</p>
+      </div>
+      <div className='gpt3__features-container'>
+      
+      </div>
+    </div>
   )
 }
 
