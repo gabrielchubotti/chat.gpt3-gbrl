aca96ab0000f5f562b08c81d7f69596b528edc5d
Gabriel Chu Bottiglieri <gabrielchubotti@gmail.com>
2023-01-16 22:30:52 -0300

    finished working on the possiblity section -- css file

--- src/containers/possibility/Possibility.jsx
+++ src/containers/possibility/Possibility.jsx
@@ -9,9 +9,9 @@ const Possibility = () => {
       <div className='gpt3__possibility-image'>
         <img src={possibilityImage} alt='possibility' />
       </div>
-      <div className='gpt__possibility-content'>
+      <div className='gpt3__possibility-content'>
         <h4>Request early access to get started</h4>
-        <h1 className='gradient-text'> The possiblities are beyong your imagination</h1>
+        <h1 className='gradient__text'> The possiblities are beyong your imagination</h1>
         <p>djfnasfj jdfnsdjfnds jf dsjfn sdjfnsdjfnsjdfj nsdjfn jsdnfjsdfn sjdf nsjfn sjdn fjsdfn sdjf nsdjf nsdj n</p>   
         <h4>Request Early Access to Get Started</h4>
       </div>
