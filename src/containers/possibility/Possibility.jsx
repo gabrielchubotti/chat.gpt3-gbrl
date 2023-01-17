7f0378921f28d7663edf8cb781c2edf1a9a21899
Gabriel Chu Bottiglieri <gabrielchubotti@gmail.com>
2023-01-16 22:21:49 -0300

    finished working on the possiblity section

--- src/containers/possibility/Possibility.jsx
+++ src/containers/possibility/Possibility.jsx
@@ -9,8 +9,15 @@ const Possibility = () => {
       <div className='gpt3__possibility-image'>
         <img src={possibilityImage} alt='possibility' />
       </div>
+      <div className='gpt__possibility-content'>
+        <h4>Request early access to get started</h4>
+        <h1 className='gradient-text'> The possiblities are beyong your imagination</h1>
+        <p>djfnasfj jdfnsdjfnds jf dsjfn sdjfnsdjfnsjdfj nsdjfn jsdnfjsdfn sjdf nsjfn sjdn fjsdfn sdjf nsdjf nsdj n</p>   
+        <h4>Request Early Access to Get Started</h4>
+      </div>
     </div>
   )
+
 }
 
 export default Possibility
