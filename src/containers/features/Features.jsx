82327dbcb5a498df99721354a620c1fbfc731fdc
Gabriel Chu Bottiglieri <gabrielchubotti@gmail.com>
2023-01-16 22:09:32 -0300

    Completed features section

--- src/containers/features/Features.jsx
+++ src/containers/features/Features.jsx
@@ -30,7 +30,7 @@ const Features = () => {
       </div>
       <div className='gpt3__features-container'>
       {featuresData.map((item, index) => (
-        <Feature />
+        <Feature title={item.title} text= {item.text} key={item.title + index}/>
       ))}
       </div>
     </div>
