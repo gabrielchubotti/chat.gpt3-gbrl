80a60d55c51ba98b9ecaccdd0e256b66baa85070
Gabriel Chu Bottiglieri <gabrielchubotti@gmail.com>
2023-01-17 22:10:21 -0300

    Started working on the Blog section

--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -3,7 +3,13 @@ import './blog.css';
 
 const Blog = () => {
   return (
-    <div>Blog</div>
+    <div className='gpt3__blog section__padding' id='blog'>
+      <div className='gpt3__blog-heading'>
+        <h1 className='gradient__text'>A lot is happening. We are blogging about it.</h1>
+
+      </div>
+
+    </div>
   )
 }
 
