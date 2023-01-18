50debeef96654c7766d3c8592a0efb3dd5f4f7a4
Gabriel Chu Bottiglieri <gabrielchubotti@gmail.com>
2023-01-17 22:24:58 -0300

    finished creating structure for blog posts inside blog section

--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import './blog.css';
+import {Article} from '../../components';
+import {blog01, blog02, blog03, blog04, blog05} from './import'
+
 
 const Blog = () => {
   return (
@@ -8,7 +11,17 @@ const Blog = () => {
         <h1 className='gradient__text'>A lot is happening. We are blogging about it.</h1>
 
       </div>
-
+      <div className='gpt3__blog-container'>
+        <div className='gpt3__blog-container_groupA'>
+          <Article imgURL={blog01} date='Jan 18, 2023' title='GPT-3 and OpenAI is the future. Let us explore it?'/>
+        </div>
+        <div className='gpt3__blog-container_groupA'>
+          <Article imgURL={blog02} date='Jan 18, 2023' title='GPT-3 and OpenAI is the future. Let us explore it?'/>
+          <Article imgURL={blog03} date='Jan 18, 2023' title='GPT-3 and OpenAI is the future. Let us explore it?'/>
+          <Article imgURL={blog04} date='Jan 18, 2023' title='GPT-3 and OpenAI is the future. Let us explore it?'/>
+          <Article imgURL={blog05} date='Jan 18, 2023' title='GPT-3 and OpenAI is the future. Let us explore it?' />
+         </div>
+      </div>
     </div>
   )
 }
