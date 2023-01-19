076e55ed5abd6a76f3bfa416e0defc9a98019bbb
Gabriel Chu Bottiglieri <gabrielchubotti@gmail.com>
2023-01-18 21:48:59 -0300

    wrote Footer jsx section

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -1,10 +1,45 @@
 import React from 'react';
 import './footer.css';
+import gpt3Logo from '../../assets/logo.svg'
 
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='gpt3__footer section__padding'>
+      <div className='gpt3__footer-heading'>
+        <h1 className='gradient__text'> Do you want to set into the future?!</h1>
+      </div>
+      <div className='gpt3__footer-btn'>
+        <p>Request Early Access</p>
+      </div>
+      <div className='gpt3__footer-links'>
+        <div className='gpt3__footer-links_logo'>
+          <img src={gpt3Logo} alt='logo'/>
+          <p>Address kmdfkf 3kd kmdfksmdfk, askamdka</p>
+        </div>
+        <div className='gpt3__footer-links_div'>
+          <h4>Links</h4>
+          <p>Overons</p>
+          <p>Social Media</p>
+          <p>Counters</p>
+          <p>Contact</p>
+        </div>
+        <div className='gpt3__footer-links_div'>
+          <h4>Company</h4>
+          <p>Terms & Conditions</p>
+          <p>Privacy Policy</p>
+          <p>Counters</p>
+          <p>Contact</p>
+        </div>
+        <div className='gpt3__footer-links_div'>
+          <h4>Links</h4>
+          <p>Overons</p>
+          <p>Social Media</p>
+          <p>Counters</p>
+          <p>Contact</p>
+        </div>
+      </div>
+    </div>
   )
 }
 
